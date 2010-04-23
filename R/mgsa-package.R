694c92e00d0179edef30f0b2d67653bab6d1bf34
Sebastian Bauer <mail@sebastianbauer.info>
2010-04-23 15:19:36 +0000

    Added file for roxygen.

---
+++ R/mgsa-package.R
@@ -0,0 +1,9 @@
+#' \tabular{11}{
+#' Package: \tab mgsa\cr
+#' Version: \tab 0.2-1\cr
+#' Date: \tab 2010-02-11\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#' @name mgsa-package
+#' @title Model-based gene set analysis
+roxygen()
