ce507ed7fd9dd5bf42d43c137c5399c24a856cce
Sebastian Bauer <mail@sebastianbauer.info>
2010-04-28 09:28:31 +0000

    This tabular stuff didn't work. Removed it for now.

--- R/mgsa-package.R
+++ R/mgsa-package.R
@@ -1,9 +1,3 @@
-#' \tabular{11}{
-#' Package: \tab mgsa\cr
-#' Version: \tab 0.2-1\cr
-#' Date: \tab 2010-02-11\cr
-#' LazyLoad: \tab yes\cr
-#' }
 #' @name mgsa-package
 #' @title Model-based gene set analysis
 #' @keywords  package
