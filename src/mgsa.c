1f909d53e4458a3766cdf1d856040621a9135bc7
Sebastian Bauer <mail@sebastianbauer.info>
2010-09-21 10:19:08 +0000

    Added scaling for prior sampling.

--- src/mgsa.c
+++ src/mgsa.c
@@ -205,10 +205,10 @@ static void parameter_prior_sample(struct prior_sample *sample, struct parameter
 {
 	double rnd = genrand(mt);
 
-	/* TODO: Add support for scaling in the continuous case */
 	if (prior->uniform_continuous)
-		sample->u.continuous_value = rnd;
-	else
+	{
+		sample->u.continuous_value = prior->uniform_continous_lower + (rnd - prior->uniform_continous_lower)*(prior->uniform_continous_upper - prior->uniform_continous_lower);
+	} else
 	{
 		sample->u.discrete_index = rnd * prior->number_of_states;
 		sample->u.discrete_index %= prior->number_of_states;
