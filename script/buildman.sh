9f066fb8ca5d71b8a8b2bb44fd49197b75f783c7
Sebastian Bauer <mail@sebastianbauer.info>
2010-10-27 11:51:00 +0000

    Now wipes out all .Rd files in the directory. Now ensures the usage of ASCII encoding.

--- script/buildman.sh
+++ script/buildman.sh
@@ -3,7 +3,9 @@
 # Builds the manual
 #
 
-R CMD roxygen -d -s `dirname $0`/..
+rm -f `dirname $0`/../man/*.Rd
+
+LANG="en.ASCII" R CMD roxygen -d -s `dirname $0`/..
 
 rm -Rf mgsa.pdf mgsa-internal.pdf
 R CMD Rd2dvi --no-preview --pdf -o mgsa.pdf `dirname $0`/..
