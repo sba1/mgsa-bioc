28446d9c121ced4f3a29c00a1469e75eab6d1e04
Sebastian Bauer <mail@sebastianbauer.info>
2013-03-07 20:34:14 +0000

    Removed deprecated .First.lib function as this is now done with the useDynLib() directive in the NAMESPACE. Now depends on R 2.14.0.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/mgsa@74028 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,3 @@
-.First.lib <- function(lib, pkg) {
-  print(paste(lib,pkg))
-
-  library.dynam("mgsa", pkg, lib)
-#  vers <- paste(sessionInfo()$otherPkg$mgsa$Version,".",sep="")
-
-  print(paste("Package mgsa initialized"))
-}
-
 .Last.lib <- function() {
   library.dynam.unload("mgsa", paste(.libPaths(),"/mgsa",sep=""))
 }
