15a2ca4d959a79574de2120256f4dcede94c668b
Sebastian Bauer <mail@sebastianbauer.info>
2014-10-17 22:03:34 +0200

    Added too simple test for readGAF.

---
+++ tests/testthat/test-readGAF.R
@@ -0,0 +1,9 @@
+test_that("readGAF() works", {
+  d<-readGAF("gene_association_head.goa_ref_human.gz")
+  expect_equal(d@numberOfItems, 184)
+})
+
+test_that("readGAF() with aspect works", {
+  d<-readGAF("gene_association_head.goa_ref_human.gz",aspect="C")
+  expect_equal(d@numberOfItems, 149)
+})
